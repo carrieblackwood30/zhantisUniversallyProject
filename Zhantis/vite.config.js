2b4fe177ae4f644b843055958e16b341137619bd
unknown <carrieblackwood30@gmail.com>
2026-02-23 13:34:18 +0500

    added code for netlify

--- Zhantis/vite.config.js
+++ Zhantis/vite.config.js
@@ -9,6 +9,9 @@ export default defineConfig({
     vue(),
     tailwindcss(),
   ],
+  build: {
+    outDir: 'dist'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
